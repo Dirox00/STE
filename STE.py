2e2f93a2b2bb8323069b97a4f1d5b0e6abddac54
Dirox00 <dirox@mailo.com>
2020-03-08 11:21:04 +0100

    Add files via upload

---
+++ STE.py
@@ -0,0 +1,151 @@
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter.simpledialog import askstring
+from tkinter import filedialog, messagebox
+from tkinter.messagebox import showinfo
+
+import base64
+import hashlib
+from Crypto.Cipher import AES
+from Crypto import Random
+ 
+BLOCK_SIZE = 16
+pad = lambda s: s.decode() + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+ 
+def encrypt(raw, password):
+    # private_key = hashlib.sha256(password.encode("utf-8")).digest()
+    # print(raw)
+    raw = raw.encode()
+    raw = pad(raw)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(password, AES.MODE_CBC, iv)
+    raw = raw.encode()
+    return base64.b64encode(iv + cipher.encrypt(raw))
+ 
+def decrypt(enc, password):
+    # private_key = hashlib.sha256(password.encode("utf-8")).digest()
+    enc = base64.b64decode(enc)
+    iv = enc[:16]
+    cipher = AES.new(password, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[16:]))
+
+def hasher(data):
+    return hashlib.sha256(data.encode("utf-8")).digest()
+
+
+global filename
+filename = ''
+
+def save(event=False):
+    global filename 
+
+    content = txt.get('1.0', END)
+    if not filename:
+        # filename = askstring(window, 'File name:')
+        filename = filedialog.asksaveasfilename(initialdir = "/home",title = "Select file's name and location",filetypes = (("Text file","*.txt"),("all files","*.*")))
+    if type(filename) != tuple and filename:    
+        window.title("Secure Text Editor-{}".format(filename))
+
+        content = encrypt(content, password).decode()
+
+        with open(filename, 'w+') as f:
+            f.write(content)
+
+def saveAs(event=False):
+    global filename 
+
+    content = txt.get('1.0', END)
+    filename = filedialog.asksaveasfilename(initialdir = "/home",title = "Select file's name and location",filetypes = (("Text file","*.txt"),("all files","*.*")))
+    
+    if type(filename) != tuple and filename:
+        window.title("Secure Text Editor-{}".format(filename))
+
+        content = encrypt(content, password).decode()
+
+        with open(filename, 'w+') as f:
+            f.write(content)
+    
+def new(event=False):
+    global filename 
+
+    filename = filedialog.asksaveasfilename(initialdir = "/home",title = "Select file's name and location",filetypes = (("Text file","*.txt"),("all files","*.*")))
+    if type(filename) != tuple and filename:
+        window.title("Secure Text Editor-{}".format(filename))
+
+        f = open(filename, 'w+')
+        f.close()
+
+def openfile(event=False):
+    global filename
+
+    filename = filedialog.askopenfilename(initialdir = "/home",title = "Select file",filetypes = (("Text file","*.txt"), ("all files","*.*")))
+
+    if type(filename) != tuple and filename:
+        window.title("Secure Text Editor-{}".format(filename))
+
+        with open(filename, 'r') as f:
+            content = f.read()
+
+        content = decrypt(content, password).decode()
+
+        txt.delete('1.0', END)
+        txt.insert('1.0', content)
+
+def exitFile():
+    msg = messagebox.askyesnocancel("Confirm", "Your work could not be saved. Do you want to save?")
+    if  msg == True:
+        save()
+        if filename:
+            window.quit()
+    elif msg == False:
+        window.quit()
+    else:
+        pass
+
+window = Tk()
+window.geometry('1000x800')
+
+inputPasswd = askstring('Password', 'Enter your password')
+global password
+password = hasher(inputPasswd)
+del inputPasswd #for not storing password in plain text
+
+
+if filename:
+    window.title("Secure Text Editor-{}".format(filename))
+else:
+    window.title('Secure Text Editor')
+
+menu = Menu(window)
+
+filemenu = Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=new)
+filemenu.add_command(label="Open", command=openfile)
+filemenu.add_separator()
+filemenu.add_command(label="Save", command=save)
+filemenu.add_command(label="Save as", command=saveAs)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=exitFile)
+
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="Commands", command=None)
+helpmenu.add_command(label="About...", command=None)
+
+window.config(menu=menu)
+
+global txt
+var = StringVar()
+txt = ScrolledText(window, padx=50, pady=70, wrap=WORD)
+txt.pack(expand=True, fill=BOTH, padx=60, pady=20)
+
+# print(txt.count('1.0', END, chars=True, update=True))
+
+window.protocol('WM_DELETE_WINDOW', exitFile)
+window.bind('<Control-s>', save)
+window.bind('<Control-o>', openfile)
+window.bind('<Control-n>', new)
+
+window.mainloop()
